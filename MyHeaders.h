2aff4e00699cf801b2bf8532fd36f20c84eb9ef8
LittleMike <LittleMike@users.noreply.github.com>
2015-08-31 23:27:56 +0800

    Create MyHeaders.h

---
+++ MyHeaders.h
@@ -0,0 +1,16 @@
+/**************************************************************************/
+#ifndef MYHEADERSH
+#define MYHEADERSH
+
+//时间转换
+#define MINTOHOUR 2
+#define HOURTODAY 12
+
+#define CARPLACENUMBER 30//车位数量
+#define PASSROADCARNUMBER 5//便道车辆数量
+#define CARNUMBERPH 20//每时刻汽车到来数量上限
+#define PROFITPH 30//停车每小时费用(元)
+#define MAX 10000//定义一个最大值
+
+#endif
+/**************************************************************************/
